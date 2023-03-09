8ed12f3c6f318f9fda134807286a8fb2b102b43f
shunying <shunying.zhang@gmail.com>
2023-03-09 16:34:55 +0800

    Hashing password

---
+++ app.js
@@ -0,0 +1,70 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const md5 = require("md5");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://0.0.0.0:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: md5(req.body.password)
+    });
+
+    newUser.save().then((result) => {
+ 
+        res.render("secrets");
+    });
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = md5(req.body.password);
+
+    User.findOne({email: username})
+    .then(function(foundUser){
+        if (foundUser) {
+            if (foundUser.password === password) {
+                res.render("secrets");
+            }
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
